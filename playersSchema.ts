03a58642201125ee5d353beabc20f42df240092a
Eko450-eng <ekrem@wipdesig.de>
2022-05-31 23:12:33 +0200

    Removed rudimentary stuffs

--- playersSchema.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const schema = new mongoose.Schema({
-    player: { type: String, required: true },
-    points: { type: Number, required: false }
-})
-
-export default mongoose.model('playerSchema', schema)
